b5fd7809acbb417d7814567a93c0eaf5e9c1e9aa
akashkhedekar8080 <108119273+akashkhedekar8080@users.noreply.github.com>
2022-06-24 13:31:01 +0530

    Add files via upload

---
+++ verificient/js/piechart.js
@@ -0,0 +1,43 @@
+
+      
+        var piechart= {
+            series: [30,50,20],
+            chart: {
+            width: 380,
+            type: 'donut',
+          },
+          plotOptions: {
+            pie: {
+              startAngle: -90,
+              endAngle: 270
+            }
+          },
+          dataLabels: {
+            enabled: false
+          },
+          fill: {
+            type: 'gradient',
+          },
+          legend: {
+            formatter: function(val, opts) {
+              return val + " - " + opts.w.globals.series[opts.seriesIndex]
+            }
+          },
+          title: {
+            // text: 'Gradient Donut with custom Start-angle'
+          },
+          responsive: [{
+            breakpoint: 480,
+            options: {
+              chart: {
+                width: 200
+              },
+              legend: {
+                position: 'bottom'
+              }
+            }
+          }]
+          };
+  
+          // var chart = new ApexCharts(document.querySelector("#chart"), options);
+          // chart.render();
